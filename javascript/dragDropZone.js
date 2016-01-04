b3073a092394e7d7827ce71ac01033e3a7bff70d
Xs54Ti <xatengui@hotmail.com>
2016-01-04 14:53:33 +0100

    testobien?

--- javascript/dragDropZone.js
+++ javascript/dragDropZone.js
@@ -2,41 +2,46 @@ $(document).ready(function() {
 
 	var mouse_down = false; 
 
+	$('.connectedGlass.healObject').on('touchstart', function(e) {
+		on_mouse_down_square(e);
+	});
+
 	function on_mouse_down_square(event) { 
-	 mouse_down=true; 
+		event.preventDefault();
+		mouse_down=true; 
 	} 
 
 	function on_mouse_up(event){ 
-	 mouse_down=false; 
+		mouse_down=false; 
 	} 
 
-	document.onmousemove = on_mouse_move; 
+	document.ontouchmove = on_mouse_move; 
 
-	document.onmouseup = on_mouse_up; 
+	document.ontouchend = on_mouse_up; 
 
 	function on_mouse_move(event) { 
 	  if (mouse_down === true) { 
-		if(event.clientX-50<=0){
-			document.querySelector('#square').style.left = 0+'px';
-		} else if(event.clientX-50>300) {
-			document.querySelector('#square').style.left = 300+'px'; 
+		if(event.touches[0].clientX-50<=0){
+			document.querySelector('.connectedGlass.healObject').style.left = 0+'px';
+		} else if(event.touches[0].clientX-50>300) {
+			document.querySelector('.connectedGlass.healObject').style.left = 300+'px'; 
 		} else {
-			document.querySelector('#square').style.left = event.clientX-50+'px';
+			document.querySelector('.connectedGlass.healObject').style.left = event.touches[0].clientX-50+'px';
 		}
 		
-		if(event.clientY-50<=0){
-			document.querySelector('#square').style.top = 0+'px';
-		} else if(event.clientY-50>300) {
-			document.querySelector('#square').style.top = 300+'px'; 
+		if(event.touches[0].clientY-50<=0){
+			document.querySelector('.connectedGlass.healObject').style.top = 0+'px';
+		} else if(event.touches[0].clientY-50>300) {
+			document.querySelector('.connectedGlass.healObject').style.top = 300+'px'; 
 		} else {
-			document.querySelector('#square').style.top = event.clientY-50+'px';
+			document.querySelector('.connectedGlass.healObject').style.top = event.touches[0].clientY-50+'px';
 		}
 		
 		
-		if(event.clientY-50>=0 && event.clientY-50<300) {
-			document.querySelector('#square').style.top = event.clientY-50+'px'; 
+		if(event.touches[0].clientY-50>=0 && event.touches[0].clientY-50<300) {
+			document.querySelector('.connectedGlass.healObject').style.top = event.clientY-50+'px'; 
 		}
 
 	  } 
-	}
-}
+	} 
+});
