363ff4e565c64a23deb6ed7ccd7ff14c1a50b9e0
Guillaume Raimbault <15801047+gRaimbault@users.noreply.github.com>
2022-03-20 01:31:46 +0100

    Update mainScript.js

--- javascript/mainScript.js
+++ javascript/mainScript.js
@@ -9,8 +9,6 @@ $(document).ready(function() {
 		}
 	});
 
-	$( "#mailToAdd" ).html( "raimbault.guillaume@gmail.com" );
-
 
 
 	//$('.info-to-know').hover($(this).next('.info-content').slideToggle(),$(this).next('.info-content').slideToggle());
